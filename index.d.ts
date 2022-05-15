79e349c401b5b4db573d53c5a1190bd33dd7c62d
d0rich <dorich2000@gmail.com>
2022-05-15 04:06:53 +0000

    Include `interface` and `type` for db types

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,62 @@
  * Model Currency
  * 
  */
+export type Currency = {
+  name: string
+  ticker: string
+}
+
+/**
+ * Model Security
+ * 
+ */
+export type Security = {
+  name: string
+  ticker: string
+  price: number
+  currency_ticker: string
+}
+
+/**
+ * Model FollowedSecurity
+ * 
+ */
+export type FollowedSecurity = {
+  security_ticker: string
+  followed_since: Date
+}
+
+/**
+ * Model PortfolioPosition
+ * 
+ */
+export type PortfolioPosition = {
+  security_ticker: string
+  amount: number
+}
+
+/**
+ * Model Operation
+ * 
+ */
+export type Operation = {
+  exchange_id: string | null
+  security_ticker: string
+  status: string
+  operation_type: string
+  amount: number
+  price: number
+  updated_at: Date
+  created_at: Date
+}
+
+
+
+
+/**
+ * Model Currency
+ * 
+ */
 export interface ICurrency = {
   name: string
   ticker: string
@@ -50,5 +106,6 @@ export interface IOperation = {
   updated_at: Date
   created_at: Date
 }
+
 
 
